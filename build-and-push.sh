a4017fe210c919352e7d7fb0ca270f0603486c64
proguy914629 <github@proguy914629.me>
2022-08-13 14:30:42 +0700

    Delete build-and-push.sh

--- build-and-push.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build -t $1 .
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 133420669210.dkr.ecr.us-east-1.amazonaws.com
-sudo docker tag $1:latest 133420669210.dkr.ecr.us-east-1.amazonaws.com/$1:latest
-sudo docker push 133420669210.dkr.ecr.us-east-1.amazonaws.com/$1:latest
