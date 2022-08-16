f46805a4b568fe49f0feb00df365e9f6df20ed18
proguy914629 <github@proguy914629.me>
2022-08-16 11:40:42 +0000

    Fix stuff

--- app.py
+++ app.py
@@ -4,23 +4,36 @@ sys.path.extend(['/tmp/', '/tmp/smg_serverless'])
 
 import json
 
+import boto3
 import numpy as np
 
 from smg_serverless.src.get_genre import main as get_genre  # type: ignore
 
 
+with open('config.json', 'r') as js:
+    conf = json.load(js)
+
+
 def handler(event, context):
-    melspectrogram = None
+    filename = None
 
     if body := event.get('body'):
         js = json.loads(body)
-        melspectrogram = js.get('melspectrogram')
+        filename = js.get('melspectrogram')
 
-    if not melspectrogram:
+    if not filename:
         return {
             'statusCode': 400,
             'body': 'No melspectrogram provided in JSON data body'
         }
+
+    s3 = boto3.client('s3', aws_access_key_id=conf["AWS_ACCESS_KEY_ID"], 
+                        aws_secret_access_key=conf["AWS_SECRET_ACCESS_KEY"],
+                        region_name=conf["AWS_REGION"], **conf.get("AWS_S3_EXTRAS", {}))
+
+    obj = s3.get_object(Bucket=conf["S3_BUCKET"], Key=filename)
+
+    melspectrogram = json.loads(obj['Body'].read().decode('utf-8'))
 
     S = np.array(melspectrogram, dtype="float32")
 
